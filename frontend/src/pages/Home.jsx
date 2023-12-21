cacad2fb141320d197269682ab854f231ef97595
Tafi <havinhtai712@gmail.com>
2023-12-21 22:23:58 +0700

    first-commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../styles/home.css';
+
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import Subtitle from '../shared/Subtitle';
+
+const Home = () => {
+    return (
+        <>
+            {/* ========== hero section start ========== */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="6">
+                            <div className="hero__content">
+                                <div className="hero__subtitle d-flex align-items-center">
+                                    <Subtitle />
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            {/* ========== hero section end ========== */}
+        </>
+    );
+};
+
+export default Home;
